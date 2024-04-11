c36f9cbb823ea146e533f41ca3d7f32f870a76b7
barbarazamperete <barbarazamperete@mprr.mp.br>
2024-04-11 17:32:07 -0400

    criar usuario rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent
-  }
+  { path: '', component: HomeComponent },
+  { path: 'cadastro/usuario', component: CriarUsuarioComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
