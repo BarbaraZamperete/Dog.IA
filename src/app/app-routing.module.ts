e9a3cfa252fbea148b7b4f050df36fa63b5cb7a2
barbarazamperete <barbarazamperete@mprr.mp.br>
2024-04-12 11:42:50 -0400

    login component logica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
 import { CriarCachorroComponent } from './criar-cachorro/criar-cachorro.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastro/usuario', component: CriarUsuarioComponent },
-  { path: 'cadastro/cachorro', component: CriarCachorroComponent }
+  { path: 'cadastro/cachorro', component: CriarCachorroComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Aqui você pode implementar a lógica para autenticar o usuário
+      const email = this.loginForm.value.email;
+      const senha = this.loginForm.value.senha;
+      console.log('Email:', email);
+      console.log('Senha:', senha);
+
+      // Após a autenticação bem-sucedida, você pode redirecionar para a próxima página
+      this.router.navigate(['/']);
+    } else {
+      // Se o formulário não for válido, você pode adicionar lógica para lidar com isso, como exibir mensagens de erro
+      console.log('Formulário inválido');
+    }
+  }
 }
