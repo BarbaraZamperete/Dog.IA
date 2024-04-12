fdbc495270d1edc0a3dde0cceb853ff16160451f
barbarazamperete <barbarazamperete@mprr.mp.br>
2024-04-12 09:37:03 -0400

    rotas para criar usuario - criar cachorro - voltar

--- src/app/criar-usuario/criar-usuario.component.ts
+++ src/app/criar-usuario/criar-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-usuario',
@@ -11,7 +12,7 @@ export class CriarUsuarioComponent {
   usuarioForm!: FormGroup;
 
   isEditable = false;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.usuarioForm = this.fb.group({
@@ -27,9 +28,11 @@ export class CriarUsuarioComponent {
     if (this.usuarioForm.valid) {
       // Aqui você pode implementar a lógica para enviar os dados do formulário para o servidor
       console.log(this.usuarioForm.value);
+      this.router.navigate(['/cadastro/cachorro']);
     } else {
       // Se o formulário não for válido, você pode adicionar lógica para lidar com isso, como exibir mensagens de erro
       console.log('Formulário inválido');
     }
+
   }
 }
