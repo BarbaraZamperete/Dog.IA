13dae3b5e1919b476b9575818d6c619ebdad75a7
BarbaraZamperete <bahzamp25@gmail.com>
2024-04-23 09:56:42 -0400

    alteração das opções do tonavbar e forma como aparecem

--- src/app/shared/components/topnavbar/topnavbar.component.ts
+++ src/app/shared/components/topnavbar/topnavbar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 
+
 @Component({
   selector: 'app-topnavbar',
   templateUrl: './topnavbar.component.html',
@@ -9,18 +10,24 @@ import { Location } from '@angular/common';
 })
 export class TopnavbarComponent {
 
+
   @Input() logo: boolean = false;
+  // @Input() menuNav: boolean = false;
 
-  logado: boolean = true
+  logado: boolean = false
+  home: boolean = false
 
-  constructor(private location: Location, private router: Router) { }
+  constructor(private location: Location, private router: Router) {
+    this.location.path() == '' ? this.home = true : this.home = false;
+    console.log(this.location.path())
+  }
 
 
-  onBack(){
+  onBack() {
     this.location.back();
   }
 
-  onNavTo(path: string){
+  onNavTo(path: string) {
     this.router.navigate([path])
   }
 }
