06105cfcff8c1b3c7b1f29f211cb8a97b2183307
BarbaraZamperete <bahzamp25@gmail.com>
2024-04-27 23:32:45 -0400

    retornar os cachorros por usuario logado

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CachorroService } from '../../services/cachorro.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,17 +13,29 @@ export class DashboardComponent {
 
   cachorrosBuscados$: Observable<any>
   cachorrosAvistados$: Observable<any>
+  usuario: string = "Usuário"
+  id: string = "1"
 
   constructor(
     private cachorroService: CachorroService,
-    private router: Router
+    private router: Router,
+    private auth: AuthService
   ) {
+    const id = auth.getUserId()
+    const username = auth.getUsername();
+    if (username !== null) {
+      this.usuario = username; // Esta atribuição agora é segura, pois garantimos que username não é nulo
+    }
+    if (id !== null){
+      this.id = id
+    }
+
     this.getCachorrosServidor()
   }
 
   getCachorrosServidor() {
-    this.cachorrosBuscados$ = this.cachorroService.getCachorrosBuscadosByUser('1')
-    this.cachorrosAvistados$ = this.cachorroService.getCachorrosAvistadosByUser('1')
+    this.cachorrosBuscados$ = this.cachorroService.getCachorrosBuscadosByUser(this.id)
+    this.cachorrosAvistados$ = this.cachorroService.getCachorrosAvistadosByUser(this.id)
   }
 
   onAdicionar(tipo: string) {
