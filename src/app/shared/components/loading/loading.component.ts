8e3cb43005b48741e25314925d960865d38e315f
BarbaraZamperete <bahzamp25@gmail.com>
2024-04-30 16:31:10 -0400

    mensagem dinamica no loading

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -7,6 +7,22 @@ import { Component, Input } from '@angular/core';
 })
 export class LoadingComponent {
 
-  @Input() mensagem: string = "carregando"
+  mensagens: string[] = ['Analisando imagens...', 'Gerando resultados...', 'Calculando semelhanças...'];
+  mensagemAtual: string = this.mensagens[0];
+  mensagemIndex: number = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    // Inicia a troca de mensagens em um intervalo de tempo
+    this.intervalId = setInterval(() => {
+      this.mensagemIndex = (this.mensagemIndex + 1) % this.mensagens.length;
+      this.mensagemAtual = this.mensagens[this.mensagemIndex];
+    }, 3000); // Troca de mensagem a cada 3 segundos (ajuste conforme desejado)
+  }
+
+  ngOnDestroy() {
+    // Limpa o intervalo quando o componente é destruído
+    clearInterval(this.intervalId);
+  }
 
 }
